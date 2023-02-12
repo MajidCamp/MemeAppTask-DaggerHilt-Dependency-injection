208add24d6b1fbd9c7f0cf8ffb342a885006ec84
MajidCamp <majid.edus@gmail.com>
2023-02-12 20:40:58 +0200

    first commit

---
+++ app/src/main/java/com/example/ktorsampleapp/model/Routes.kt
@@ -0,0 +1,10 @@
+package com.example.ktorexamplelab.model
+
+
+object Routes {
+    private const val Base_MEME = " https://meme-api.com/gimme"
+    const val WHOLESOME = "$Base_MEME/wholesome/6"
+    const val DARKSOULS = "$Base_MEME/darksouls/6"
+    const val ANIMAL = "$Base_MEME/FunnyAnimals/6"
+
+}
